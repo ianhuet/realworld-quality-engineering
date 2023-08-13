c450e7bfc6de0ea619207ccd37005f66d46907d0
Ian Huet <ian@huet.info>
2023-08-13 15:22:24 +0100

    add deploy Render action

--- packages/backend/jest.config.ts
+++ packages/backend/jest.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
